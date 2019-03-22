c8fc495e11cf5cc21d45ad4331228ed28a086deb
Ric Taglietti <rictaglietti@gmail.com>
2019-03-22 09:54:04 -0400

    working on group page

--- client/src/components/pages/GroupPage.jsx
+++ client/src/components/pages/GroupPage.jsx
@@ -9,7 +9,7 @@ class Group extends Component {
     items: [],
     receiptName: '',
     owner: '',
-    guests: '',
+    guests: [],
     imgPath: ''
   };
 
@@ -21,40 +21,75 @@ class Group extends Component {
           receiptName: res.data.receiptName,
           items: res.data.items,
           owner: res.data.owner,
-          guests: res.data.guests,
+          guests: res.data.guests.split(','),
           imgPath: res.data.imgPath
         })
       })
+
+      //this.splitGuests()
   }
 
   showItems = () => {
     return this.state.items.map((item, i) => {
       return (
-        <div key={i} className="group-items">
-          <div>
-            {`${item[0]}: $${item[1]}`}
+        <div className="group-items">
+          <div className="food-bubble">
+          <div key={i}>{`${item[0]}: $${item[1]}`}</div>
+          <button>+</button>
           </div>
         </div>
-      )
+      );
     })
   }
 
-  splitGuests = () => {
-    console.log(this.state.guests)
+  // splitGuests = () => {
+  //   console.log('HIIIIIIIIIII')
+  //   let str = [...this.state.guests]
+  //   console.log('yooyo', str)
+  //   //let strClean = str.replace(/\s/g, '')
+  //   //console.log('replaced', strClean)
+  //   let guestArr = []
+  //   guestArr = str.split(' ')
+  //   console.log('hi', guestArr)
+    
+  //   this.setState({
+  //     guestArr
+  //   })
+  // }
+
+  showGuests = () => {
+    let guestList = [...this.state.guests]
+    console.log('hjijhrije', this)
+    return guestList.map((guest, i) => {
+      return (
+        <div key={i}>
+          {guest}
+        </div>
+      )
+    })
+    //return guestList
   }
 
   render() {
     console.log('this.state', this.state)
     // const {receiptName, items, owner, guests} = {...this.state}
     return (
-      <div className="Group">Group {this.state.receiptName}
-        {/* <div>{owner}</div>
-        <div>{guests}</div>
-        <div>{items}</div> */}
-        <div>{this.showItems()}</div>
-        <div>{this.splitGuests()}</div>
+      <div>
+        <div className="Group">
+          <h2>Group {this.state.receiptName}</h2>
+          {/* <div>{owner}</div>
+          <div>{guests}</div>
+          <div>{items}</div> */}
+          <div className="items-row">{this.showItems()}</div>
+          {this.showGuests()}
+        </div>
+        {/* <div>
+          <div>
+            {this.showGuests()}
+          </div>
+        </div> */}
       </div>
-    )
+    );
   }
 }
 
